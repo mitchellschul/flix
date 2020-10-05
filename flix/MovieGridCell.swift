25c07b19d3d17044e7668e0b0cf91c65be0a63a7
Mitchell Schuldinger <mitchellasdf@gmail.com>
2020-10-05 13:10:06 -0400

    Added tab bar navigation Added grid view of superhero movies

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Mitchell Schuldinger on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
